c7338cc5b8545f40657ba1a66633e27606521486
Gonzaslo Ariel Biasin <gonzalobiasin@gmail.com>
2026-05-05 01:36:54 -0300

    fix nombre login

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import Login from "./Login";
-import Dashboard from "./Dashboard";
+import Dashboard from "./components/Dashboard";
 
 function App() {
   const [user, setUser] = useState(null);
